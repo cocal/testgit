1d83da6007aa742b77268fa264c6d7eefaa833e8
unknown <cocaltzq@gmail.com>
2014-05-08 14:28:29 +0800

    test conn

---
+++ src/base/APStimer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014-4-11
+
+@author: zqtang
+'''
+from apscheduler.scheduler import Scheduler
+
+__sch = Scheduler()
+
+def start():
+    print('sch is start...')
---
+++ src/view/contorlCenter.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014-4-11
+
+@author: zqtang
+'''
+from base import APStimer
+
+APStimer.start()
